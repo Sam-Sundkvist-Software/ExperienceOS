fea84f283ed5e3d59b3a58a2fabcc4ab62884fa5
sundkvistdev <162899660+sundkvistdev@users.noreply.github.com>
2026-08-29 16:52:46 +0300

    Introduced bases for AppDRT and BootCore

---
+++ public/src/system/core/AppDRT.ts
@@ -0,0 +1,10 @@
+/**
+ * The Application Dearchival Runtime (ADRT) for ExperienceOS.
+ */
+export default class AppDRT implements IADRT {
+
+}
+
+export interface IADRT {
+
+}
---
+++ public/src/system/core/BootCore.ts
@@ -0,0 +1,10 @@
+/**
+ * The boot-up manager (BootCore) for ExperienceOS.
+ */
+export default class BootCore implements IBootCore {
+
+}
+
+export interface IBootCore {
+
+}
