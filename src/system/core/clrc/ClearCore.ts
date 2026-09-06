9989af36d3d928b2a56c13e5e16da213bf7d5be6
sundkvistdev <162899660+sundkvistdev@users.noreply.github.com>
2026-09-06 12:26:57 +0300

    Continued ClearCore development

--- src/system/core/clrc/ClearCore.ts
+++ src/system/core/clrc/ClearCore.ts
@@ -3,23 +3,182 @@
  * The improved central (UI) component framework for ExperienceOS.
  */
 export default class ClearCore implements IClearCore {
+	private _apps: { [id: string]: IClearCoreApp; };
 
 	public constructor() {
-
+		this._apps = {};
 	}
 
+	public addApp(app: IClearCoreApp): string {
+		if (!app || !(app instanceof ClearCoreApp))
+			throw new ClearCoreError("App not supported.");
+
+		const id = app.id;
+
+		if (this._apps[id])
+			throw new ClearCoreError("App with an identical ID already registered.");
+
+		this._apps[id] = app;
+
+		return id;
+	}
+
+	public removeApp(id: string): void {
+		if (!this._apps[id])
+			return;
+
+		delete this._apps[id];
+	}
+
+	public startApp(id: string): void {
+		const app = this._apps[id];
+
+		if (!app)
+			throw new ClearCoreError("App not found.");
+
+		app.start();
+	}
+
+	public stopApp(id: string): void {
+		const app = this._apps[id];
+
+		if (!app)
+			throw new ClearCoreError("App not found.");
+
+		app.stop();
+	}
+
+	public appExists(id: string): boolean {
+		return !!this._apps[id];
+	}
+
+	public isAppRunning(id: string): boolean {
+		const app = this._apps[id];
+
+		if (!app)
+			throw new ClearCoreError("App not found.");
+
+		return app.isRunning();
+	}
 }
 
 export interface IClearCore {
+	/**
+	 * Adds an app to the ClearCore database.
+	 * @returns The app ID.
+	 */
+	addApp(app: IClearCoreApp): string;
+	removeApp(id: string): void;
+	startApp(id: string): void;
+	stopApp(id: string): void;
+	appExists(id: string): boolean;
+	isAppRunning(id: string): boolean;
+}
 
-	
+export class ClearCoreError extends Error {
+	public constructor(message?: string, options?: ErrorOptions) {
+		super(message, options);
+		this.name = "ClearCoreError";
+	}
+}
 
+export class ClearCoreApp implements IClearCoreApp {
+	private _config: IClearCoreAppConfig;
+	private _actionFunctions: { [id: string]: (...args: unknown[]) => void; }
+
+	private _id: string;
+	private _isRunning: boolean;
+
+	public get id() {
+		return this._id;
+	}
+
+	public constructor(config: IClearCoreAppConfig) {
+		this._config = config;
+		this._actionFunctions = {};
+
+		this._validateConfig();
+
+		this._id = this._config.meta.id;
+		this._isRunning = false;
+	}
+
+	private _validateConfig() {
+		// TODO: Validate config
+	}
+
+	public start(): void {
+		if (this._isRunning)
+			return;
+
+		this._isRunning = true;
+	}
+
+	public stop(): void {
+		if (!this._isRunning)
+			return;
+
+		this._isRunning = false;
+	}
+
+	public isRunning(): boolean {
+		return this._isRunning;
+	}
+
+	public addActionFunction(id: string, func: (...args: unknown[]) => unknown): void {
+		// TODO: throw?
+		if (!func || typeof func !== "function")
+			throw new ClearCoreError("The provided function is invalid.");
+
+		if (this._actionFunctions[id])
+			throw new ClearCoreError("Cannot replace a function with the same id.");
+
+		this._actionFunctions[id] = func;
+	}
+
+	public removeActionFunction(id: string): void {
+		if (!this._actionFunctions[id])
+			return;
+
+		delete this._actionFunctions[id];
+	}
+
+	public invokeActionFunction(id: string, ...args: unknown[]): unknown {
+		const func = this._actionFunctions[id];
+
+		if (!func || typeof func !== "function")
+			throw new ClearCoreError("A function by the provided id does not exist or is invalid.");
+
+		return func(...args);
+	}
+
+	public actionFunctionExists(id: string): boolean {
+		const func = this._actionFunctions[id];
+		return !!func && typeof func === "function";
+	}
+}
+
+export interface IClearCoreApp {
+	readonly id: string;
+	start(): void;
+	stop(): void;
+	isRunning(): boolean;
+	addActionFunction(id: string, func: (...args: unknown[]) => unknown): void;
+	removeActionFunction(id: string): void;
+	invokeActionFunction(id: string, ...args: unknown[]): unknown;
+	actionFunctionExists(id: string): boolean;
 }
 
 export interface IClearCoreAppConfig {
 
+	/**
+	 * The mandatory ClearCore type property.
+	 */
 	type: "clrcApp";
 
+	/**
+	 * The ClearCore version to use.
+	 */
 	version: string;
 
 	/**
@@ -66,16 +225,34 @@ export interface IClearCoreAppConfig {
 		};
 	};
 
-	viewContent: {
+	/**
+	 * The primary view content. When left undefined, no useful UI is drawn.
+	 */
+	viewContent?: ClearCoreViewControl;
 
-	}
+	/**
+	 * The action definitions, such as for button clicks or link clicks.
+	 */
+	actions: {
+		[actionId: string]: ClearCoreAction;
+	};
+
 }
+
+export interface IClearCoreAction {
+	// TODO: Implement action system
+}
+
+/**
+ * An invocable action. Either a TS/JS function identifier or a declarative JSON action.
+ */
+export type ClearCoreAction = string | IClearCoreAction;
 
 export enum ClearCoreViewControlType {
 	DEFAULT = "",
 
 	// Regular
-	PANEL = "PANEL",
+	PANEL = "panel",
 	LABEL = "label",
 	BUTTON = "button",
 	INPUT = "input",
@@ -84,6 +261,11 @@ export enum ClearCoreViewControlType {
 
 export interface IClearCoreViewControl {
 	type: ClearCoreViewControlType;
+
+	/**
+	 * The ID of the view control, used for retrieving values or setting text.
+	 */
+	id?: string;
 
 	/**
 	 * @default true
